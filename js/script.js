b85e6d842d7d93927d48ff636eeb147edc4f1f80
federicaarosio <arosio.fede@gmail.com>
2023-11-24 15:42:51 +0100

    js: changed strings in img

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // Creare l’array di oggetti con le informazioni fornite.
 const teamMembers = [
-    {name: "Wayne Barnett", role: "Founder & CEO", photo: "./img/wayne-barnett-founder-ceo.jpg" },
-    {name: "Angela Caroll", role: "Chief Editor", photo: "./img/angela-caroll-chief-editor.jpg" },
-    {name: "Walter Gordon", role: "Office Manager", photo: "./img/walter-gordon-office-manager.jpg" },
-    {name: "Angela Lopez", role: "Social Media Manager", photo: "./img/angela-lopez-social-media-manager.jpg" },
-    {name: "Scott Estrada", role: "Developer", photo: "./img/scott-estrada-developer.jpg" },
-    {name: "Barbara Ramos", role: "Graphic Designer", photo: "./img/barbara-ramos-graphic-designer.jpg" },		
+    {name: "Wayne Barnett", role: "Founder & CEO", photo: "wayne-barnett-founder-ceo.jpg" },
+    {name: "Angela Caroll", role: "Chief Editor", photo: "angela-caroll-chief-editor.jpg" },
+    {name: "Walter Gordon", role: "Office Manager", photo: "walter-gordon-office-manager.jpg" },
+    {name: "Angela Lopez", role: "Social Media Manager", photo: "angela-lopez-social-media-manager.jpg" },
+    {name: "Scott Estrada", role: "Developer", photo: "scott-estrada-developer.jpg" },
+    {name: "Barbara Ramos", role: "Graphic Designer", photo: "barbara-ramos-graphic-designer.jpg" },		
 ];
 
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
@@ -17,11 +17,12 @@ for (let i = 0; i < teamMembers.length; i++) {
         
     const containerEl = document.querySelector("section.team-members-wrapper");
     const teamCardEl = document.createElement("article");
+    teamCardEl.classList.add("card");
     teamCardEl.innerHTML = 
     `
     <div class="name">${teamMember.name}</div>
     <div class="role">${teamMember.role}</div>
-    <div class="photo">${teamMember.photo}</div>
+    <img src="./img/${teamMember.photo}" alt="team member photo">
     `
     containerEl.appendChild(teamCardEl);
 }
